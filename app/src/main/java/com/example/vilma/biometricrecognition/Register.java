1afcd2e2fd02830c573c7263d8c1e50a65e85884
jclewis94 <jclewis8312@eagle.fgcu.edu>
2018-01-21 23:48:24 -0500

    Added comments and pulled the methods we might need to upload to the S3 Bucket.

--- app/src/main/java/com/example/vilma/biometricrecognition/Register.java
+++ app/src/main/java/com/example/vilma/biometricrecognition/Register.java
@@ -1,18 +1,30 @@
 package com.example.vilma.biometricrecognition;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.ContentUris;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Environment;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferObserver;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -30,6 +42,19 @@ public class Register extends BaseActivity {
     Button btnRegister;
     private String mCurrentPhotoPath;
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    // TAG for logging;
+    private static final String TAG = "UploadActivity";
+
+    //Below is taken from https://docs.aws.amazon.com/sdkfornet1/latest/apidocs/html
+    //                             /T_Amazon_S3_Transfer_TransferUtility.htm
+    // The TransferUtility is the primary class for managing transfer to S3
+    //TransferUtility provides a simple API for uploading content to and downloading content from
+    // Amazon S3. It makes extensive use of Amazon S3 multipart uploads to achieve enhanced
+    // throughput, performance, and reliability. When uploading large files by specifying file
+    // paths instead of a stream, TransferUtility uses multiple threads to upload multiple parts
+    // of a single upload at once. When dealing with large content sizes and high bandwidth, this
+    // can increase throughput significantly.
+    private TransferUtility transferUtility;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -97,5 +122,122 @@ public class Register extends BaseActivity {
             }
         }
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        //checks to see if the activity is "all good"
+        if (resultCode == Activity.RESULT_OK) {
+
+            //grabs the info from the intents data and set the uri to that data
+            Uri uri = data.getData();
+
+            //this tries to begin the upload to the s3 bucket using the path of the picture selected
+            try {
+                //grabs path of picture
+                String path = getPath(uri);
+
+                //calls the begin method, and sends the path of the picture we are uploading.
+                beginUpload(path);
+            } catch (URISyntaxException e) {
+                Toast.makeText(this,
+                        "Unable to get the file from the given URI.  See error log for details",
+                        Toast.LENGTH_LONG).show();
+                Log.e(TAG, "Unable to upload file from the given uri", e);
+            }
+        }
+    }
+
+    private void beginUpload(String filePath) {
+        //makes sure there is a filePath sent
+        if (filePath == null) {
+            Toast.makeText(this, "Could not find the filepath of the selected file",
+                    Toast.LENGTH_LONG).show();
+            return;
+        }
+        //creates a file with the filepath
+        File file = new File(filePath);
+
+        //begins the uploading using the transferUtility object initalized at the start of this
+        //class
+        TransferObserver observer = transferUtility.upload(Constants.BUCKET_NAME, file.getName(),
+                file);
+
+        //comments below were created by amazon
+        /*
+         * Note that usually we set the transfer listener after initializing the
+         * transfer. However it isn't required in this sample app. The flow is
+         * click upload button -> start an activity for image selection
+         * startActivityForResult -> onActivityResult -> beginUpload -> onResume
+         * -> set listeners to in progress transfers.
+         */
+        // observer.setTransferListener(new UploadListener());
+    }
+
+    @SuppressLint("NewApi")
+    private String getPath(Uri uri) throws URISyntaxException {
+        final boolean needToCheckUri = Build.VERSION.SDK_INT >= 19;
+        String selection = null;
+        String[] selectionArgs = null;
+        // Uri is different in versions after KITKAT (Android 4.4), we need to
+        // deal with different Uris.
+        if (needToCheckUri && DocumentsContract.isDocumentUri(getApplicationContext(), uri)) {
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                return Environment.getExternalStorageDirectory() + "/" + split[1];
+            } else if (isDownloadsDocument(uri)) {
+                final String id = DocumentsContract.getDocumentId(uri);
+                uri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+            } else if (isMediaDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+                if ("image".equals(type)) {
+                    uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+                selection = "_id=?";
+                selectionArgs = new String[] {
+                        split[1]
+                };
+            }
+        }
+        if ("content".equalsIgnoreCase(uri.getScheme())) {
+            String[] projection = {
+                    MediaStore.Images.Media.DATA
+            };
+            Cursor cursor = null;
+            try {
+                cursor = getContentResolver()
+                        .query(uri, projection, selection, selectionArgs, null);
+                int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                if (cursor.moveToFirst()) {
+                    return cursor.getString(column_index);
+                }
+            } catch (Exception e) {
+            }
+        } else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+        return null;
+    }
+
+    public static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    public static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    public static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
+    }
+
 
 }
