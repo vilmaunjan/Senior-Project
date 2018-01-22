6fa25f6437e42fbb3ef8cb3ef893220ac96548cf
vilmaunjan <vrunjan0014@eagle.fgcu.edu>
2018-01-22 00:18:04 -0500

    Add Util and ComparePics classes

--- app/src/main/java/com/example/vilma/biometricrecognition/ComparePictures.java
+++ app/src/main/java/com/example/vilma/biometricrecognition/ComparePictures.java
@@ -12,6 +12,11 @@ import com.amazonaws.AmazonClientException;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.regions.Regions;
 //import com.amazonaws.services.rekognition.model.AmazonRekognitionException;
+import com.amazonaws.services.rekognition.model.BoundingBox;
+import com.amazonaws.services.rekognition.model.CompareFacesMatch;
+import com.amazonaws.services.rekognition.model.CompareFacesRequest;
+import com.amazonaws.services.rekognition.model.CompareFacesResult;
+import com.amazonaws.services.rekognition.model.ComparedFace;
 import com.amazonaws.services.rekognition.model.DetectLabelsRequest;
 import com.amazonaws.services.rekognition.model.DetectLabelsResult;
 import com.amazonaws.services.rekognition.model.Image;
@@ -25,46 +30,42 @@ public class ComparePictures {
 
     private AmazonRekognitionClient rekognitionClient;
     private Float similarityThreshold = 80F;
-    String photo = "photo.jpg";
+    //String photo = "photo.jpg";
     String bucket = "S3bucket";
 
-    private void comparePic(Context context) {
-   /*
-    AWSCredentials credentials;
-    try {
-        credentials = new ProfileCredentialsProvider("AdminUser").getCredentials();
-    } catch(Exception e) {
-        throw new AmazonClientException("Cannot load the credentials from the"
-                + "credential profiles file. "
-                + "Please make sure that your credentials file is at the correct "
-                + "location (/Users/userid/.aws/credentials), and is in a valid format.", e);
-    }
-    AmazonRekognition rekognitionClient = AmazonRekognitionClientBuilder
-            .standard()
-            .withRegion(Regions.US_WEST_2)
-            .withCredentials(new AWSStaticCredentialsProvider(credentials))
-            .build();
-   */
-    /*
+    private void comparePic(Context context, String source, String target) {
+
         rekognitionClient = Util.getRekognitionClient(context);
 
-        DetectLabelsRequest request = new DetectLabelsRequest()
-                .withImage(new Image()
+        CompareFacesRequest request = new CompareFacesRequest()
+                .withSourceImage(new Image()
                         .withS3Object(new S3Object()
-                                .withName(photo).withBucket(bucket)))
-                .withMaxLabels(10)
-                .withMinConfidence(75F);
-        try {
-            DetectLabelsResult result = rekognitionClient.detectLabels(request);
-            List<Label> labels = result.getLabels();
-            System.out.println("Detected labels for " + photo);
-            for (Label label : labels) {
-                System.out.println(label.getName() + ": " +
-                        label.getConfidence().toString());
-            }
-        } catch (AmazonRekognitionException e) {
-            e.printStackTrace();
-        }*/
+                                .withName(source).withBucket(bucket)))
+                .withTargetImage(new Image()
+                        .withS3Object(new S3Object()
+                                .withName(target).withBucket(bucket)))
+                .withSimilarityThreshold(similarityThreshold);
+
+        // Call operation
+        CompareFacesResult compareFacesResult=rekognitionClient.compareFaces(request);
+
+        // Display results
+        List <CompareFacesMatch> faceDetails = compareFacesResult.getFaceMatches();
+        for (CompareFacesMatch match: faceDetails){
+            ComparedFace face= match.getFace();
+            BoundingBox position = face.getBoundingBox();
+            System.out.println("Face at " + position.getLeft().toString()
+                    + " " + position.getTop()
+                    + " matches with " + face.getConfidence().toString()
+                    + "% confidence.");
+
+        }
+        List<ComparedFace> uncompared = compareFacesResult.getUnmatchedFaces();
+
+        System.out.println("There were " + uncompared.size()
+                + " that did not match");
+        System.out.println("Source image rotation: " + compareFacesResult.getSourceImageOrientationCorrection());
+        System.out.println("target image rotation: " + compareFacesResult.getTargetImageOrientationCorrection());
     }
 
 }
