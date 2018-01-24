81eee06a4ca40c3b79ecb7fa45b6bda2fa0295a3
vilmaunjan <vrunjan0014@eagle.fgcu.edu>
2018-01-24 01:19:40 -0500

    Update ComparePictures and HomeActivity

--- app/src/main/java/com/example/vilma/biometricrecognition/HomeActivity.java
+++ app/src/main/java/com/example/vilma/biometricrecognition/HomeActivity.java
@@ -1,7 +1,12 @@
 package com.example.vilma.biometricrecognition;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBMapper;
 
 /**
@@ -13,9 +18,25 @@ import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBMapper;
  */
 public class HomeActivity extends BaseActivity {
 
+    Button btnCheckout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        initUI();
+    }
+
+    private void initUI() {
+        btnCheckout = (Button) findViewById(R.id.btnCheckout);
+
+        btnCheckout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ComparePictures j = new ComparePictures(HomeActivity.this,"My_Face.jpg", "My_Face2.jpg");
+                j.execute();
+                //ComparePictures.comparePic(HomeActivity.this,"My_Face.jpg", "My_Face2.jpg");
+            }
+        });
     }
 }
