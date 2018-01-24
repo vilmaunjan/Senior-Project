eab4f11c9f0ab2a245034ddd69818da9e444f935
vilmaunjan <vrunjan0014@eagle.fgcu.edu>
2018-01-24 01:18:53 -0500

    Update ComparePictures and HomeActivity

--- app/src/main/java/com/example/vilma/biometricrecognition/ComparePictures.java
+++ app/src/main/java/com/example/vilma/biometricrecognition/ComparePictures.java
@@ -5,6 +5,10 @@ package com.example.vilma.biometricrecognition;
  */
 
 import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import android.content.Context;
 
 import com.amazonaws.services.rekognition.AmazonRekognition;
 import com.amazonaws.services.rekognition.AmazonRekognitionClient;
@@ -26,16 +30,67 @@ import com.amazonaws.services.s3.model.S3DataSource;
 
 import java.util.List;
 
-public class ComparePictures {
+public class ComparePictures extends AsyncTask<Object, String, String> {
 
-    private AmazonRekognitionClient rekognitionClient;
-    private Float similarityThreshold = 80F;
+    private Context mContext;
+    private String mSource;
+    private String mTarget;
+    private String confidence;
+    private static AmazonRekognitionClient rekognitionClient;
+    private static Float similarityThreshold = 80F;
+    private static String bucket = Constants.BUCKET_NAME;
     //String photo = "photo.jpg";
     //String source = "My_Face.jpg";
     //String source = "My_Face2.jpg";
-    String bucket = Constants.BUCKET_NAME;
 
-    private void comparePic(Context context, String source, String target) {
+    public ComparePictures(Context context, String source, String target){
+        mContext = context;
+        mSource = source;
+        mTarget = target;
+    }
+
+    @Override
+    protected String doInBackground(Object... params) {
+        //String source = (String)params[0];
+        //String target = (String)params[1];
+
+        rekognitionClient = Util.getRekognitionClient(mContext);
+
+        CompareFacesRequest request = new CompareFacesRequest()
+                .withSourceImage(new Image()
+                        .withS3Object(new S3Object()
+                                .withName(mSource).withBucket(bucket)))
+                .withTargetImage(new Image()
+                        .withS3Object(new S3Object()
+                                .withName(mTarget).withBucket(bucket)))
+                .withSimilarityThreshold(similarityThreshold);
+
+        // Call operation
+        CompareFacesResult compareFacesResult=rekognitionClient.compareFaces(request);
+
+        // Display results
+        List <CompareFacesMatch> faceDetails = compareFacesResult.getFaceMatches();
+        for (CompareFacesMatch match: faceDetails){
+            ComparedFace face= match.getFace();
+            BoundingBox position = face.getBoundingBox();
+            confidence = face.getConfidence().toString();
+            System.out.println("Face at " + position.getLeft().toString()
+                    + " " + position.getTop()
+                    + " matches with " + face.getConfidence().toString()
+                    + "% confidence.");
+            //Toast.makeText(mContext,face.getConfidence().toString()+"% confidence.",Toast.LENGTH_SHORT).show();
+        }
+
+        return null;
+    }
+
+
+    protected void onPostExecute(String result) {
+        Toast toast = Toast.makeText(this.mContext, confidence, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+/*
+    public static void comparePic(Context context, String source, String target) {
 
         rekognitionClient = Util.getRekognitionClient(context);
 
@@ -60,7 +115,7 @@ public class ComparePictures {
                     + " " + position.getTop()
                     + " matches with " + face.getConfidence().toString()
                     + "% confidence.");
-
+            Toast.makeText(context,face.getConfidence().toString()+"% confidence.",Toast.LENGTH_SHORT).show();
         }
         List<ComparedFace> uncompared = compareFacesResult.getUnmatchedFaces();
 
@@ -68,6 +123,11 @@ public class ComparePictures {
                 + " that did not match");
         System.out.println("Source image rotation: " + compareFacesResult.getSourceImageOrientationCorrection());
         System.out.println("target image rotation: " + compareFacesResult.getTargetImageOrientationCorrection());
+
+
+        //Toast.makeText(context,"hi",Toast.LENGTH_SHORT).show();
+
     }
+*/
 
 }
